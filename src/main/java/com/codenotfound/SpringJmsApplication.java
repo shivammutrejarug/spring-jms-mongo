ba95d438a49a8a0b539c3768dd534e6b3381c011
shivammutrejarug <54889304+shivammutrejarug@users.noreply.github.com>
2020-12-22 00:08:41 +0100

    spring mongo working.

---
+++ src/main/java/com/codenotfound/SpringJmsApplication.java
@@ -0,0 +1,38 @@
+package com.codenotfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codenotfound.jms.Sender;
+
+@SpringBootApplication
+@RestController
+public class SpringJmsApplication {
+
+  @Autowired
+  private CustomerRepository repository;
+
+  @Autowired
+  private Sender sender;
+
+  @RequestMapping(value = "/save")
+  public void save() {
+    repository.deleteAll();
+
+    // save a couple of customers
+    repository.save(new Customer("Alice", "Babu"));
+    repository.save(new Customer("Bob", "Nath"));
+  }
+
+  @RequestMapping(value = "/available")
+  public void available() {
+    sender.send("Hello from controller!");
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJmsApplication.class, args);
+  }
+}
